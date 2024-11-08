0e2dad243a0980e27274da6ed78b2e297d131e42
Umberto Tramontano <u.tramontano92@libero.it>
2024-11-08 16:15:41 +0100

    update JS

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ console.log(saveBtn)
 saveBtn.addEventListener('click', () => {
   window.location.href = './homepage.html'
 })
+const langSelected = document.getElementById('langDrop')
+const languages = document.querySelectorAll('#langList li')
+console.log(languages)
+
+for (let i = 0; i < languages.length; i++) {
+  languages[i].addEventListener('click', () => {
+    langSelected.innerText = languages[i].innerText
+  })
+}
